a589795b8104dc41774098756ee395fbe3b6396e
pachanganepranish <pachanganepranish@gmail.com>
2025-08-17 22:51:08 +0530

    Create day_3.cpp
    day 3 challenge

---
+++ day_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int>& arr) {
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    slow = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    return slow;
+}
+
+int main() {
+    vector<int> arr = {3,1,3,4,2};
+    cout << "Duplicate number: " << findDuplicate(arr);
+}
